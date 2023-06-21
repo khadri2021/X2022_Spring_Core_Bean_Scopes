937a7c1e3b79f32fb54bed6258e3632c1eef65ce
khadri2021 <khadri.institutions@gmail.com>
2023-06-21 08:39:59 +0530

    singleton and prototype

---
+++ src/main/java/com/khadri/spring/core/Main.java
@@ -0,0 +1,23 @@
+package com.khadri.spring.core;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Bean1 bean1 = context.getBean("b1",Bean1.class);
+        System.out.println(bean1);
+
+        Bean1 bean2 = context.getBean("b1",Bean1.class);
+        System.out.println(bean2);
+
+        Bean2 bean3 = context.getBean("b2",Bean2.class);
+        System.out.println(bean3);
+
+        Bean2 bean4 = context.getBean("b2",Bean2.class);
+        System.out.println(bean4);
+    }
+}
